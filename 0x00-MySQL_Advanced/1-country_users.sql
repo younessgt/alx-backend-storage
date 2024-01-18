77c705d95c1d9a2c50dff1290bd0f51c4f0e6414
younessgt <youness.merzak123@gmail.com>
2024-01-18 13:15:26 +0100

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- creating a new table called users
+-- with id, email, name attributes
+CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+        country ENUM('US', 'CO', 'TN') NOT NULL
+);
