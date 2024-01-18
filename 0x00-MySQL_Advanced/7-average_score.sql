bf257271ca21ca991ebcd7dd4813d95f5d878544
younessgt <youness.merzak123@gmail.com>
2024-01-18 17:49:57 +0100

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,14 @@
 --script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student 
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-DELIMITER // 
+
+DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
   DECLARE new_average FLOAT;
   SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id INTO new_average;
-  
+
   UPDATE users SET average_score = new_average WHERE id = user_id;
 
 END //
