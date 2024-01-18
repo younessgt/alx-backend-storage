ececac2665cdaa4866b4f09e75d2e8d78f204a58
younessgt <youness.merzak123@gmail.com>
2024-01-18 14:43:29 +0100

    task3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- script that lists all bands with Glam rock as their main style 
+-- ranked by their longevity
+
+SELECT 
+  band_name,
+  (COALESCE(split, 2022) - formed) AS lifespan
+FROM
+  metal_bands
+WHERE
+  style LIKE '%Glam rock%'
+ORDER BY
+  lifespan DESC;
