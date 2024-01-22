af30f33fc5372d131f763fb35c9cbdd14e0bd3d8
younessgt <youness.merzak123@gmail.com>
2024-01-22 19:17:38 +0100

    task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" script contain schools_by_topic function"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """  function that returns the list of
+    school having a specific topic"""
+    topic = {"topics": topic}
+    return [doc for doc in mongo_collection.find(topic)]
