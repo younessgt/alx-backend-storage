5b8cd6bc78d7f4e202dc214b1ca21f98c55fea31
younessgt <youness.merzak123@gmail.com>
2024-01-22 18:12:30 +0100

    task8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ script contain list_all function"""
-import 
 
 
-def list_all(mongo_collection) {
-    
-}
+def list_all(mongo_collection):
+    """  function that lists all documents in a collection """
+    list_doc = [doc for doc in mongo_collection.find()]
+    return list_doc
