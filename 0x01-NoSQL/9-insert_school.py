5992b9b6d46b4b73e19066de2af758c6b02e5332
younessgt <youness.merzak123@gmail.com>
2024-01-22 18:36:57 +0100

    tsak9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" script contain insert_school function"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ function that inserts a new document
+    in a collection based on kwargs """
+    obj = {}
+    for key, value in kwargs.items():
+        obj[key] = value
+    mongo_collection.insert_one(obj)
+    for objec in mongo_collection.find(obj):
+        return objec.get('_id')
