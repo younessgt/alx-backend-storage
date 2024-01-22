eaf35c95800671638e0503c0baaeab8b159211b2
younessgt <youness.merzak123@gmail.com>
2024-01-22 22:35:40 +0100

    task 101

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" srcipt contain top_students function"""
+
+
+def top_students(mongo_collection):
+    """ function that returns all students sorted by average score """
+    list_topic = []
+    for student in mongo_collection.find():
+        topics = student.get("topics", [])
+        average_s = 0
+        if topics:
+            sum_score = 0
+            for topic in topics:
+                sum_score += float(topic.get("score", 0))
+            average_s = sum_score / len(topics)
+        mongo_collection.update_one({"_id": student["_id"]},
+                                    {"$set": {"averageScore": average_s}})
+    return mongo_collection.find().sort("id", -1)
