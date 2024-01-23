012138cc568f451418eb9a4907d81bf7f71c088e
younessgt <youness.merzak123@gmail.com>
2024-01-23 14:52:44 +0100

    task 101 with aggregation

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,7 +4,8 @@
 
 def top_students(mongo_collection):
     """ function that returns all students sorted by average score """
-    list_topic = []
+
+    """list_topic = []
     for student in mongo_collection.find():
         topics = student.get("topics", [])
         average_s = 0
@@ -13,4 +14,20 @@ def top_students(mongo_collection):
             average_s = sum_score / len(topics)
         mongo_collection.update_one({"_id": student["_id"]},
                                     {"$set": {"averageScore": average_s}})
-    return list(mongo_collection.find().sort("id", -1))
+    return mongo_collection.find().sort("id", -1)"""
+
+    list_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "topics": "$topics",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {
+                "averageScore": -1
+            }
+        }
+    ])
+    return list_student
